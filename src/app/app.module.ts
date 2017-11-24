ee1397a54e458c2a05eb9fb9de11820786f193dc
einarhreinsson <einarlogi14@gmail.com>
2017-11-24 23:44:13 +0200

    Service structure according to angular style-guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule }    from './core/core.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
   ],
   providers: [],
+  exports: [ AppComponent ],
+  entryComponents: [ AppComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
