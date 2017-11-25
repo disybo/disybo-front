d7d3a02a47c7363ab16e121efe6aa5bffe84f3b9
reginbald <reginbald@users.noreply.github.com>
2017-11-25 13:38:49 +0200

    Created ApiService and example on how to used it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ExampleApiService } from './exampleApi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Musdash';
-  constructor() {}
+
+  constructor(private exampleService: ExampleApiService) {}
+
+  ngOnInit(): void {
+    this.exampleService.hello();
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { ApiService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
  
 @NgModule({
-  imports: [],
+  imports: [
+    HttpClientModule
+  ],
   exports: [],
   declarations: [],
-  providers: [ApiService]
+  providers: []
 })
 export class CoreModule { }
