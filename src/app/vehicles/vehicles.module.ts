c5acca17055708c52eff64aa398f69f87fc4fdbc
reginbald <reginbald@users.noreply.github.com>
2017-11-25 14:48:40 +0200

    fuel consumption charts update

--- src/app/vehicles/vehicles.module.ts
+++ src/app/vehicles/vehicles.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { ChartsModule } from 'ng2-charts';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { VehiclesComponent } from './vehicles.component';
 import { FuelConsumptionComponent } from './fuel-consumption/fuel-consumption.component';
+import { VehiclesFilterComponent } from './vehicles-filter/vehicles-filter.component';
+import { VehiclesPollutionComponent } from './vehicles-pollution/vehicles-pollution.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
     ChartsModule,
+    FlexLayoutModule
   ],
-  declarations: [VehiclesComponent, FuelConsumptionComponent],
+  declarations: [VehiclesComponent, FuelConsumptionComponent, VehiclesFilterComponent, VehiclesPollutionComponent],
   exports: [VehiclesComponent]
 })
 export class VehiclesModule { }
