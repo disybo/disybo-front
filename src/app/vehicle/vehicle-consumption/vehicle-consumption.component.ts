3ff768d755239c360e36861df52a4a50de8808df
reginbald <reginbald@users.noreply.github.com>
2017-11-25 22:32:34 +0200

    Displaying a chart for vehicle consumption

--- src/app/vehicle/vehicle-consumption/vehicle-consumption.component.ts
+++ src/app/vehicle/vehicle-consumption/vehicle-consumption.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-consumption.component.css']
 })
 export class VehicleConsumptionComponent implements OnInit {
+   // lineChart
+   public lineChartData:Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartType:string = 'line';
+  public lineChartOptions:any = {
+    legend: {
+      display: false
+    },
+    responsive: true
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
--- src/app/vehicle/vehicle.module.ts
+++ src/app/vehicle/vehicle.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { AgmCoreModule } from '@agm/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { VehicleComponent } from './vehicle.component';
 import { VehicleLocationComponent } from './vehicle-location/vehicle-location.component';
@@ -11,6 +12,7 @@ import { VehicleConsumptionComponent } from './vehicle-consumption/vehicle-consu
 @NgModule({
   imports: [
     CommonModule,
+    ChartsModule,
     FlexLayoutModule,
     MatCardModule,
     AgmCoreModule
