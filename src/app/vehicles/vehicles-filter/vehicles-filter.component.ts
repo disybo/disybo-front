bde8d8f0456afd98d4625b5f976e072537c0076f
einarhreinsson <einarlogi14@gmail.com>
2017-11-25 21:26:22 +0200

    template for filtering vehicles

--- src/app/vehicles/vehicles-filter/vehicles-filter.component.ts
+++ src/app/vehicles/vehicles-filter/vehicles-filter.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicles-filter.component.css']
 })
 export class VehiclesFilterComponent implements OnInit {
-
+  // default value for radio buttons day/month/year
+  granularity = 0
   constructor() { }
 
   ngOnInit() {
   }
-
+  firstDate(x) {
+    console.log("first date got ", x)
+  }
+  endfunc(x) {
+    console.log("end date got ", x)
+  }
+  refreshfunc(x, y) {
+    console.log("refresh")
+    console.log("x and y", x, y)
+  }
 }
